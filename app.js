0e7fad02a9dbec5ecc9e6cf7fc79e2a3dcfd0921
darrenrhodes72-glitch <darren.rhodes72@ntlworld.com>
2026-06-12 20:11:48 +0100

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-let currentAssessment={hazards:[],controls:[]};
-function newAssessment(){
-document.getElementById('dashboard').style.display='none';
-document.getElementById('wizard').style.display='block';
-document.getElementById('wizard').innerHTML='<h2>New Assessment</h2>';
-}
